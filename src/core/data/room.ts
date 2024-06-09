3851c9aa38d47983caa28d89005f6ed981b71d60
Lucas Wang <lucaschiwang@gmail.com>
2024-06-09 22:24:41 +1000

    Updated Room to add users and roomType.

--- src/core/data/room.ts
+++ src/core/data/room.ts
@@ -1,14 +1,32 @@
 import { Model, Schema, Types, model } from "mongoose";
 
+enum RoomType {
+  Private = "PRIVATE",
+  Public = "PUBLIC",
+}
 export interface IRoom {
+  users: Types.ObjectId[];
   name: string;
+  roomType: string;
 }
 
 interface IRoomModel extends Model<IRoom> {}
 
 const IRoomSchema = new Schema<IRoom, IRoomModel>({
+  users: {
+    type: [Schema.Types.ObjectId],
+    ref: "user",
+    required: true,
+    default: [],
+  },
   name: {
     type: String,
+    required: true,
+  },
+  roomType: {
+    type: String,
+    enum: Object.values(RoomType),
+    default: RoomType.Public,
     required: true,
   },
 });
